5e717e222ff86e41be8365c54d996712720c8cc9
Tadjeddine Dalil <dalil.tadjeddine@gmail.com>
2025-09-04 12:54:40 +0200

    Better camera
    Better camera tracking system + alterations to basic test map

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,6 +1,14 @@
 extends Camera3D
 
+var target_position = Vector3()
+var tracking = false
+
+func _process(delta: float) -> void:
+	if tracking:
+		global_position = global_position.lerp(target_position,30*delta)
+	
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("Camera_zone"):
-		global_position = area.global_position
-		print("cac")
+		tracking = true
+		target_position = area.global_position
