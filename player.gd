798e9813aed5e3769aaa6f1c52a157ed6047f70e
Youscer <youscer@gmail.com>
2025-09-05 18:12:42 +0200

    fix movement (move_and_slide)

--- player.gd
+++ player.gd
@@ -5,44 +5,43 @@ extends CharacterBody3D
 @export var speed = 15
 @export var friction : float = 13
 var acceleration : Vector3 = Vector3.ZERO
-var dash_speed = 100
+var dash_speed = 150
 var dash_time = 0
 var dashpos = Vector3()
 var movable = true
 var click_position = Vector2()
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func dash(dashpos: Vector3):
 	movable = false
 	self.dashpos = dashpos
 	dashpos.y = 0
 
-func _process(delta):
-	velocity = Vector3()
-	var pos = Vector3()
-	
+func _process(delta):	
 	if movable:
-		input_move2(delta)
+		input_move(delta)
 	else:
-		dash_time += delta 
-		pos = position
-		pos.y = 0
-		if (pos-dashpos).length() < 0.5 || dash_time >0.3:
+		dash_time += delta
+		dashpos.y = position.y
+		if (position-dashpos).length() < 0.5 || dash_time >0.3:
+			# Si le dash est fini
 			movable = true
 			dash_time = 0
+			velocity = Vector3.ZERO
 		else:
-			velocity = (dashpos - position).normalized() * dash_speed * delta
-	move_and_collide(velocity)
+			velocity = (dashpos - position).normalized() * dash_speed
+#	move_and_collide(velocity)
+	if not is_on_floor():
+		velocity.y -= gravity
+
+	move_and_slide()
 
 func input_move(delta):
-	var traction = controller.move_vector()
-	acceleration += traction * acceleration * delta
-#	velocity -= velocity * friction * delta
-
-func input_move2(delta):
 	var direction = controller.move_vector()
 	if direction:
-		acceleration = direction * speed * delta
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		acceleration.x = lerp(acceleration.x, 0.0, delta * friction)
-		acceleration.z = lerp(acceleration.z, 0.0, delta * friction)
-	velocity += acceleration
+		velocity.x = lerp(velocity.x, 0.0, delta * friction)
+		velocity.z = lerp(velocity.z, 0.0, delta * friction)
