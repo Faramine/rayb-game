7a56e6372c8684448d184e096bf9321f59a85efb
Tadjeddine Dalil <dalil.tadjeddine@gmail.com>
2025-09-13 18:40:49 +0200

    Added populating to map generator, etc...
    Added layout application system to generation
    added boss room placement to generation
    added Orb object and orb placement
    added layout base
    added temporary start room layout

--- scripts/generator_base.gd
+++ scripts/generator_base.gd
@@ -3,9 +3,12 @@ extends Node
 
 @export var map_size = 11
 
-func generate_map(world : World) -> Dictionary:
-	var rooms : Dictionary = Dictionary()
-	var room_list : Array = Array()
+@onready var start_layout = preload("res://scenes/rooms/layouts/start_room_layout_1.tscn")
+@onready var layout = preload("res://scenes/rooms/layouts/base_room_layout.tscn")
+@onready var orb = preload("res://scenes/rooms/elements/interactables/orb.tscn")
+
+func generate_map(world : World):
+	var room_list = Array()
 	var map = Array()
 	var map_center = map_size/2
 	
@@ -29,46 +32,101 @@ func generate_map(world : World) -> Dictionary:
 		map_opened = false
 		list = room_list.duplicate()
 		for k in list:
-			if k[0]+1 < map_size && map[k[0]+1][k[1]] == 0:
-				map_opened = true
-				if randf() < -1*float(list.size())/10+11/10:
-					map[k[0]+1][k[1]] = 1
-					room_list.push_back([k[0]+1,k[1]])
-				else:
-					map[k[0]+1][k[1]] = -1
-			if k[0]-1 >= 0 && map[k[0]-1][k[1]] == 0:
-				map_opened = true
-				if randf() < -1*float(list.size())/10+11/10:
-					map[k[0]-1][k[1]] = 1
-					room_list.push_back([k[0]-1,k[1]])
-				else:
-					map[k[0]-1][k[1]] = -1
-			if k[1]+1 < map_size && map[k[0]][k[1]+1] == 0:
-				map_opened = true
-				if randf() < -1*float(list.size())/10+11/10:
-					map[k[0]][k[1]+1] = 1
-					room_list.push_back([k[0],k[1]+1])
-				else:
-					map[k[0]][k[1]+1] = -1
-			if k[1]-1 >= 0 && map[k[0]][k[1]-1] == 0:
-				map_opened = true
-				if randf() < -1*float(list.size())/10+11/10:
-					map[k[0]][k[1]-1] = 1
-					room_list.push_back([k[0],k[1]-1])
-				else:
-					map[k[0]][k[1]-1] = -1
+			map_opened = verify_room_placements(k,map,list,room_list)
 		
 		map_closed = !map_opened
 		
+	#Placing the rooms
+	
 	for k in room_list:
 		new_room = room_scene.instantiate()
 		new_room.position.x = 26.5 * (k[0]-map_center)
 		new_room.position.z = 26.5 * (k[1]-map_center)
 		new_room.coords = k
 		new_room.set_world(world)
-		rooms.set(k, new_room)
+		world.rooms.set(k, new_room)
 		add_child(new_room)
 		for r in room_list:
 			new_room.open_wall(r)
 	
-	return rooms
+	world.start_room = world.rooms.get([map_center,map_center])
+	
+	var bossable_room = Array()
+	
+	#placing the boss room
+	for k in room_list:
+		if k[0]-1 >= 0 && map[k[0]-1][k[1]] == -1:
+			bossable_room.push_back(k)
+	
+	var preboss = bossable_room[randi_range(0,bossable_room.size()-1)]
+	world.preboss_room = world.rooms.get(preboss)
+	room_list.push_back([preboss[0]-1,preboss[1]])
+	world.preboss_room.open_wall([preboss[0]-1,preboss[1]])
+	print(preboss)
+	
+	new_room = room_scene.instantiate()
+	new_room.position.x = 26.5 * (preboss[0]-1-map_center)
+	new_room.position.z = 26.5 * (preboss[1]-map_center)
+	new_room.coords = [preboss[0]-1,preboss[1]]
+	new_room.set_world(world)
+	world.rooms.set([preboss[0]-1,preboss[1]], new_room)
+	world.boss_room = new_room
+	add_child(new_room)
+	new_room.open_wall(preboss)
+
+	#placing the layouts
+	list = room_list.duplicate()
+	list.erase(preboss)
+	list.erase([preboss[0]-1,preboss[1]])
+	list.erase([map_center,map_center])
+	
+	world.start_room.populate(start_layout.instantiate())
+	
+	for k in room_list:
+		world.rooms.get(k).populate(layout.instantiate())
+	#placing the orbs
+	list = room_list.duplicate()
+	list.erase(preboss)
+	list.erase([preboss[0]-1,preboss[1]])
+	list.erase([map_center,map_center])
+		
+	var key
+	var room
+	var orbi : Orb
+	for i in range(0,3):
+		key = list[randi_range(0,list.size()-1)]
+		list.erase(key)
+		room = world.rooms.get(key)
+		orbi = orb.instantiate()
+		orbi.position = room.orb_position
+		room.add_child(orbi)
+		print(room.orb_position)
+		print(key)
+	
+	
+func verify_room_placements(k,map,list,room_list):
+	var map_opened = false
+	if k[1]-1 >= 0 && map[k[0]][k[1]-1] == 0:
+		map_opened = true
+		if place_room(k[0],k[1]-1,map,list) == 1:
+			room_list.push_back([k[0],k[1]-1])
+	if k[1]+1 <= map.size() && map[k[0]][k[1]+1] == 0:
+		map_opened = true
+		if place_room(k[0],k[1]+1,map,list) == 1:
+			room_list.push_back([k[0],k[1]+1])
+	if k[0]-1 >= 0 && map[k[0]-1][k[1]] == 0:
+		map_opened = true
+		if place_room(k[0]-1,k[1],map,list) == 1:
+			room_list.push_back([k[0]-1,k[1]])
+	if k[0]+1 <= map.size() && map[k[0]+1][k[1]] == 0:
+		map_opened = true
+		if place_room(k[0]+1,k[1],map,list) == 1:
+			room_list.push_back([k[0]+1,k[1]])
+	return map_opened
+	
+func place_room(x,y,map,list):
+	if randf() < -float(list.size())/10.0+11.0/10.0:
+		map[x][y] = 1
+	else:
+		map[x][y] = -1
+	return map[x][y]
