9cfff34a0fbfe189220fae0328269302b3c84367
Youscer <youscer@gmail.com>
2025-09-18 14:51:22 +0200

    Fixed enemy not moving at same speed on different FPS

--- scripts/player.gd
+++ scripts/player.gd
@@ -81,7 +81,6 @@ func _on_area_entered(area: Area3D) -> void:
 		is_in_godray = true
 		godray_entered.emit()
 
-
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area.is_in_group("Godray"):
 		is_in_godray = false
