d6de78d8e16bf137d89a108b4f804bcd57ab64a4
Tadjeddine Dalil <dalil.tadjeddine@gmail.com>
2025-09-18 21:02:34 +0200

    clean up

--- scenes/rooms/elements/interactables/orb.gd
+++ scenes/rooms/elements/interactables/orb.gd
@@ -1,7 +1,7 @@
 class_name Orb
 extends Node3D
 
-var invicible = false
+var inv = false
 var inv_timer = 0.0
 
 var spawn_position
@@ -14,19 +14,16 @@ func _ready() -> void:
 	animationplayer.play("Orb_floating")
 
 func _process(delta: float) -> void:
-	position = position.lerp(spawn_position, delta) + velocity
-	velocity.lerp(Vector3.ZERO,delta)
+	position = position.lerp(spawn_position , delta) + velocity
+	velocity = velocity.lerp(Vector3.ZERO,delta)
 	inv_timer += delta
 	if inv_timer >= 4.0:
-		invicible = false
+		inv = false
 
 func add_velocity(velocity : Vector3):
 	self.velocity += velocity
-	self.invincible = true
+	self.inv = true
 	self.inv_timer = 0.0
 
-func _on_area_3d_area_entered(hitbox: HitBox) -> void:
-	if hitbox == null: return
-	if not self.invincible:
-		add_velocity(Vector3(5.0,0.0,5.0))
-	print("caca")
+func take_damage(damage):
+	add_velocity(Vector3(0.1,0.0,0.1))
