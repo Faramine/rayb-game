8cc71a5ba2ed259df44f7730643e43b7a61728c2
Tadjeddine Dalil <dalil.tadjeddine@gmail.com>
2025-09-19 01:05:59 +0200

    orb animation

--- scenes/rooms/elements/interactables/orb.gd
+++ scenes/rooms/elements/interactables/orb.gd
@@ -5,6 +5,11 @@ var inv = false
 var inv_timer = 0.0
 
 var spawn_position
+var strength = 2.0
+
+@onready var outer_mesh : MeshInstance3D = $MeshInstance3D/MeshInstance3D2
+@onready var inner_mesh : MeshInstance3D = $MeshInstance3D
+
 @onready var velocity = Vector3(0.0,0.0,0.0)
 
 @onready var animationplayer = $AnimationPlayer
@@ -13,17 +18,31 @@ func _ready() -> void:
 	spawn_position = position
 	animationplayer.play("Orb_floating")
 
-func _process(delta: float) -> void:
-	position = position.lerp(spawn_position , delta) + velocity
-	velocity = velocity.lerp(Vector3.ZERO,delta)
-	inv_timer += delta
-	if inv_timer >= 4.0:
-		inv = false
-
-func add_velocity(velocity : Vector3):
-	self.velocity += velocity
-	self.inv = true
-	self.inv_timer = 0.0
-
-func take_damage(damage):
-	add_velocity(Vector3(0.1,0.0,0.1))
+func take_damage(hitbox : HitBox):
+	var tween = create_tween()
+	var tween2 = create_tween()
+	var albedo = outer_mesh.mesh.material.albedo_color
+	
+	var direction = (global_position - hitbox.global_position).normalized()
+	var interpolated_position = global_position + direction * strength
+	var interpolated_position2 = global_position + direction * strength / 2
+	
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self,"global_position",interpolated_position,1.0)
+	
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self,"global_position",interpolated_position2,0.75)
+	
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self,"global_position",global_position,2)
+	
+	tween2.set_ease(Tween.EASE_OUT)
+	tween2.set_trans(Tween.TRANS_QUAD)
+	tween2.tween_property(outer_mesh.mesh.material,"albedo_color",Color(10,10,10),0.1)
+	
+	tween2.set_ease(Tween.EASE_OUT)
+	tween2.set_trans(Tween.TRANS_QUAD)
+	tween2.tween_property(outer_mesh.mesh.material,"albedo_color",albedo,0.3)
