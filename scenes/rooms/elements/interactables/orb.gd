adc5126faf46b2f63c4faddf67f6a32990cc01f8
Tadjeddine Dalil <dalil.tadjeddine@gmail.com>
2025-09-18 19:02:34 +0200

    post processing test
    various changes

--- scenes/rooms/elements/interactables/orb.gd
+++ scenes/rooms/elements/interactables/orb.gd
@@ -1,7 +1,11 @@
 class_name Orb
 extends Node3D
 
-var spawn_position;
+var invicible = false
+var inv_timer = 0.0
+
+var spawn_position
+@onready var velocity = Vector3(0.0,0.0,0.0)
 
 @onready var animationplayer = $AnimationPlayer
 
@@ -10,4 +14,19 @@ func _ready() -> void:
 	animationplayer.play("Orb_floating")
 
 func _process(delta: float) -> void:
-	position = position.lerp(spawn_position, delta)
+	position = position.lerp(spawn_position, delta) + velocity
+	velocity.lerp(Vector3.ZERO,delta)
+	inv_timer += delta
+	if inv_timer >= 4.0:
+		invicible = false
+
+func add_velocity(velocity : Vector3):
+	self.velocity += velocity
+	self.invincible = true
+	self.inv_timer = 0.0
+
+func _on_area_3d_area_entered(hitbox: HitBox) -> void:
+	if hitbox == null: return
+	if not self.invincible:
+		add_velocity(Vector3(5.0,0.0,5.0))
+	print("caca")
